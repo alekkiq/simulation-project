ef595061d27038e904d611ebd0933daa871749dd
Temetski33 <teemu.laasio@metropolia.fi>
2025-10-04 23:44:01 +0300

    Add first database variables

--- src/simu/model/SimulationData.java
+++ src/simu/model/SimulationData.java
@@ -3,4 +3,11 @@ package simu.model;
 public class SimulationData {
     // add instance variables HERE
 
+    private int receptionServed = 0;
+    private int receptionServers = 1;
+    private double receptionAvgWait = 0;
+    private double receptionAvgService = 0;
+    private double receptionAvgTotal = 0;
+    private double receptionUtil = 0;
+
 }
