c67e5eca032c32797570a7720948fe13844ecac7
Temetski33 <teemu.laasio@metropolia.fi>
2025-10-04 23:47:05 +0300

    Add mechanic and wash variables

--- src/simu/model/SimulationData.java
+++ src/simu/model/SimulationData.java
@@ -10,4 +10,19 @@ public class SimulationData {
     private double receptionAvgTotal = 0;
     private double receptionUtil = 0;
 
+    private int mechanicServed = 0;
+    private int mechanicServers = 2;
+    private double mechanicAvgWait = 0;
+    private double mechanicAvgService = 0;
+    private double mechanicAvgTotal = 0;
+    private double mechanicUtil = 0;
+
+    private int washServed = 0;
+    private int washServers = 1;
+    private double washAvgWait = 0;
+    private double washAvgService = 0;
+    private double washAvgTotal = 0;
+    private double washUtil = 0;
+
+
 }
