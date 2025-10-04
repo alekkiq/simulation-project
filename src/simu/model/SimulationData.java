8bb899cefb61e8876193f52c0a7f661e5bff6b83
Temetski33 <teemu.laasio@metropolia.fi>
2025-10-05 00:04:02 +0300

    Add setters and getters

--- src/simu/model/SimulationData.java
+++ src/simu/model/SimulationData.java
@@ -1,7 +1,6 @@
 package simu.model;
 
 public class SimulationData {
-    // add instance variables HERE
 
     private int receptionServed = 0;
     private int receptionServers = 1;
@@ -25,4 +24,147 @@ public class SimulationData {
     private double washUtil = 0;
 
 
+    public int getReceptionServed() {
+        return receptionServed;
+    }
+
+    public void setReceptionServed(int receptionServed) {
+        this.receptionServed = receptionServed;
+    }
+
+    public int getReceptionServers() {
+        return receptionServers;
+    }
+
+    public void setReceptionServers(int receptionServers) {
+        this.receptionServers = receptionServers;
+    }
+
+    public double getReceptionAvgWait() {
+        return receptionAvgWait;
+    }
+
+    public void setReceptionAvgWait(double receptionAvgWait) {
+        this.receptionAvgWait = receptionAvgWait;
+    }
+
+    public double getReceptionAvgService() {
+        return receptionAvgService;
+    }
+
+    public void setReceptionAvgService(double receptionAvgService) {
+        this.receptionAvgService = receptionAvgService;
+    }
+
+    public double getReceptionAvgTotal() {
+        return receptionAvgTotal;
+    }
+
+    public void setReceptionAvgTotal(double receptionAvgTotal) {
+        this.receptionAvgTotal = receptionAvgTotal;
+    }
+
+    public double getReceptionUtil() {
+        return receptionUtil;
+    }
+
+    public void setReceptionUtil(double receptionUtil) {
+        this.receptionUtil = receptionUtil;
+    }
+
+    public int getMechanicServed() {
+        return mechanicServed;
+    }
+
+    public void setMechanicServed(int mechanicServed) {
+        this.mechanicServed = mechanicServed;
+    }
+
+    public int getMechanicServers() {
+        return mechanicServers;
+    }
+
+    public void setMechanicServers(int mechanicServers) {
+        this.mechanicServers = mechanicServers;
+    }
+
+    public double getMechanicAvgWait() {
+        return mechanicAvgWait;
+    }
+
+    public void setMechanicAvgWait(double mechanicAvgWait) {
+        this.mechanicAvgWait = mechanicAvgWait;
+    }
+
+    public double getMechanicAvgService() {
+        return mechanicAvgService;
+    }
+
+    public void setMechanicAvgService(double mechanicAvgService) {
+        this.mechanicAvgService = mechanicAvgService;
+    }
+
+    public double getMechanicAvgTotal() {
+        return mechanicAvgTotal;
+    }
+
+    public void setMechanicAvgTotal(double mechanicAvgTotal) {
+        this.mechanicAvgTotal = mechanicAvgTotal;
+    }
+
+    public double getMechanicUtil() {
+        return mechanicUtil;
+    }
+
+    public void setMechanicUtil(double mechanicUtil) {
+        this.mechanicUtil = mechanicUtil;
+    }
+
+    public int getWashServed() {
+        return washServed;
+    }
+
+    public void setWashServed(int washServed) {
+        this.washServed = washServed;
+    }
+
+    public int getWashServers() {
+        return washServers;
+    }
+
+    public void setWashServers(int washServers) {
+        this.washServers = washServers;
+    }
+
+    public double getWashAvgWait() {
+        return washAvgWait;
+    }
+
+    public void setWashAvgWait(double washAvgWait) {
+        this.washAvgWait = washAvgWait;
+    }
+
+    public double getWashAvgService() {
+        return washAvgService;
+    }
+
+    public void setWashAvgService(double washAvgService) {
+        this.washAvgService = washAvgService;
+    }
+
+    public double getWashAvgTotal() {
+        return washAvgTotal;
+    }
+
+    public void setWashAvgTotal(double washAvgTotal) {
+        this.washAvgTotal = washAvgTotal;
+    }
+
+    public double getWashUtil() {
+        return washUtil;
+    }
+
+    public void setWashUtil(double washUtil) {
+        this.washUtil = washUtil;
+    }
 }
