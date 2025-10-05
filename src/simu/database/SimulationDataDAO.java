8e4cc805d8d513b97a31e159cad7dbeb38b523b1
Temetski33 <teemu.laasio@metropolia.fi>
2025-10-05 09:06:40 +0300

    Add instance variables to persist

--- src/simu/database/SimulationDataDAO.java
+++ src/simu/database/SimulationDataDAO.java
@@ -6,15 +6,35 @@ import java.sql.*;
 import java.util.*;
 
 public class SimulationDataDAO {
-    public void persist(SimulationData emp) {
+    public void persist(SimulationData data) {
         Connection conn = MariaDBConnection.getConnection();
-        String sql = "INSERT INTO simulation_data (first_name, last_name, email, salary) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO simulation_data (" +
+                "receptionServed, receptionServers, receptionAvgWait, receptionAvgService, receptionAvgTotal, receptionUtil, " +
+                "mechanicServed, mechanicServers, mechanicAvgWait, mechanicAvgService, mechanicAvgTotal, mechanicUtil, " +
+                "washServed, washServers, washAvgWait, washAvgService, washAvgTotal, washUtil" +
+                ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try {
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, emp.getFirstName());
-            ps.setString(2, emp.getLastName());
-            ps.setString(3, emp.getEmail());
-            ps.setDouble(4, emp.getSalary());
+            ps.setInt(1, data.getReceptionServed());
+            ps.setInt(2, data.getReceptionServers());
+            ps.setDouble(3, data.getReceptionAvgWait());
+            ps.setDouble(4, data.getReceptionAvgService());
+            ps.setDouble(5, data.getReceptionAvgTotal());
+            ps.setDouble(6, data.getReceptionUtil());
+
+            ps.setInt(7, data.getMechanicServed());
+            ps.setInt(8, data.getMechanicServers());
+            ps.setDouble(9, data.getMechanicAvgWait());
+            ps.setDouble(10, data.getMechanicAvgService());
+            ps.setDouble(11, data.getMechanicAvgTotal());
+            ps.setDouble(12, data.getMechanicUtil());
+
+            ps.setInt(13, data.getWashServed());
+            ps.setInt(14, data.getWashServers());
+            ps.setDouble(15, data.getWashAvgWait());
+            ps.setDouble(16, data.getWashAvgService());
+            ps.setDouble(17, data.getWashAvgTotal());
+            ps.setDouble(18, data.getWashUtil());
 
             ps.executeUpdate();
         } catch (SQLException e) {
