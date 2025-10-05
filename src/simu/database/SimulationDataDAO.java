34ed0439a633cb81daa323ffa697e4667da4bb49
Temetski33 <teemu.laasio@metropolia.fi>
2025-10-05 08:59:37 +0300

    Add initial version of persist

--- src/simu/database/SimulationDataDAO.java
+++ src/simu/database/SimulationDataDAO.java
@@ -1,6 +1,25 @@
 package simu.database;
 
+
+import simu.model.SimulationData;
+import java.sql.*;
+import java.util.*;
+
 public class SimulationDataDAO {
-    // persist HERE
+    public void persist(SimulationData emp) {
+        Connection conn = MariaDBConnection.getConnection();
+        String sql = "INSERT INTO simulation_data (first_name, last_name, email, salary) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, emp.getFirstName());
+            ps.setString(2, emp.getLastName());
+            ps.setString(3, emp.getEmail());
+            ps.setDouble(4, emp.getSalary());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
