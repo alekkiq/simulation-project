7e7f7e9f693d5722005127a483ee48f888a567ba
Temetski33 <teemu.laasio@metropolia.fi>
2025-10-05 06:19:10 +0300

    Add mariadb connection

--- src/simu/database/MariaDBConnection.java
+++ src/simu/database/MariaDBConnection.java
@@ -1,5 +1,38 @@
 package simu.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MariaDBConnection {
-    // maria db onnection here
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn==null) {
+            // connect if necessary
+            try {
+                conn = DriverManager.getConnection(
+                        "jdbc:mariadb://localhost:3306/company?user=appuser&password=password");
+            } catch (SQLException e) {
+                System.out.println("Connection failed.");
+                e.printStackTrace();
+            }
+            return conn;
+        }
+        else {
+            return conn;
+        }
+    }
+
+    public static void terminate() {
+        try {
+            getConnection().close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
 }
